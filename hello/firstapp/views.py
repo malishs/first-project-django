cf4d64237ad0b574e3ef1ff66686e9510df3dafc
Your Name <you@example.com>
2024-09-11 14:34:20 +0300

    11.09.2024. Нагорный/Ларин. ЛЗ4. В этой лекции делали маршруты для нашего проекта.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
 
 # Create your views here.
-
-from django.http import HttpResponse
   
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
- 
+    return HttpResponse("<h2>Глaвнaя</h2>")
+
 def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
- 
+    return HttpResponse("<h2>0 сайте</h2>")
+
 def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
+    return HttpResponse("<h2>Koнтaкты</h2>")
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, re_path
+from django.urls import path
+from django.urls import re_path
 from firstapp import views
 
 urlpatterns = [
-    re_path(r'^about/contact/', views.contact),
-    re_path(r'^about', views.about),
     path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
 ]
+
